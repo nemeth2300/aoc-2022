d0a31c5fcb039c5d483f39a2ffee36a9c44b9d39
nemeth <nemeth2300@gmail.com>
2022-12-09 02:47:52 +0100

    AOC 6

---
+++ 6/AOC_2022_06-2.f90
@@ -0,0 +1,41 @@
+! Not my produest code
+
+program main
+   implicit none
+
+   integer, parameter :: INPUT_MAX_LENGTH = 4096
+   integer, parameter :: MARKER_LENGTH = 14
+
+   integer, parameter :: FILE_PATH_MAX_SIZE = 1024
+   integer, parameter :: FILE_UNIT = 10
+
+   character (len=FILE_PATH_MAX_SIZE), parameter :: file_path = "input.txt"
+
+   character (len=INPUT_MAX_LENGTH) :: input
+   character (len=MARKER_LENGTH) :: current_input
+   integer :: index
+
+   integer :: i, j
+
+   open(unit=FILE_UNIT, file=file_path, status="old")
+   read(unit=FILE_UNIT, fmt=*) input
+   close(FILE_UNIT)
+
+   main_loop: do index=1, INPUT_MAX_LENGTH
+      current_input = input(index:index+MARKER_LENGTH)
+
+      do i=1,MARKER_LENGTH
+         do j=1,MARKER_LENGTH
+            if (i /=j .AND. current_input(i:i) == current_input(j:j)) then
+               cycle main_loop
+            end if
+         end do
+      end do
+
+      exit
+   end do main_loop
+
+   print *, index+MARKER_LENGTH-1
+
+end program main
+
